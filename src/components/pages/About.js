1f354f01e375e171534480965ef6f95afac5c2fc
Brennan M <brennanmcvicar@gmail.com>
2017-03-10 00:57:00 -0500

    Add credits to about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import AboutText from '../AboutText'
+import aboutStyles from '../../css/about.scss'
 import mobilizeLogoBig from '../../assets/images/about/mobilize-logo-big.jpg'
 
 function About({about, route}) {
@@ -11,6 +12,37 @@ function About({about, route}) {
 			<div className="about-container">
 				<img src={mobilizeLogoBig} className="about-img" alt="Mobilize Logo" />
 				<AboutText />
+			</div>
+			<h3>Credits</h3>
+			<div className="credits">
+				{[
+					"Luke Allen",
+					"Matt Bockelman",
+					"Zoe Brock",
+					"Jiyoon Cha",
+					"Cory Choy",
+					"Justin Frankel",
+					"Lily Hamburger",
+					"Brian Herman",
+					"Thomas Johnson",
+					"Alexander Juhan",
+					"Kyle Kaglund",
+					"Jason Katz",
+					"Donny Kim",
+					"Charlotte Littlehales",
+					"Nick Long",
+					"Brennan McVicar",
+					"Kristin Mink",
+					"Betsy Nagler",
+					"Claudio Santos",
+					"Aaron Schillinger",
+					"Jon Selby",
+					"Eric Shansby",
+					"Fletcher Wolfe",
+					"Rachel Yood"
+				].map(
+					name => <span>{name}</span>
+				)}
 			</div>
 		</div>
 	);
