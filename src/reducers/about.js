2d7050ceec4b351aff38c5a9382fb1c41fdd6e33
Brennan M <brennanmcvicar@gmail.com>
2017-04-03 20:16:56 -0400

    Partial application of RSS

--- src/reducers/about.js
+++ src/reducers/about.js
@@ -1,4 +1,7 @@
 const defaultState = {
+	name: 'Mobilize',
+	email: 'mobilizehere@gmail.com',
+	website: 'https://www.mobilizehere.com',
 	aboutText: [
 		'Mobilize is a podcast that puts a spotlight on and is a resource for people, friends, communities, and activists who have decided to stand up, resist, fight back-- mobilize.', 
 		'Each day, together, we shine a light on the truth; we focus on the things that unite us; we pull each other up; we celebrate our shared humanity.'
