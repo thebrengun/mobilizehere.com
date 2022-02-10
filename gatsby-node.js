f44c22a8cfdd295ee62c072fe66a514646c24666
Brennan <thebrengun@gmail.com>
2022-02-09 23:36:48 -0500

    fix: create schema for frontmatter field duration
      - stop breaking build from duration being inferred as non-string

--- gatsby-node.js
+++ gatsby-node.js
@@ -156,3 +156,16 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     })
   }
 }
+
+exports.createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions
+  const typeDefs = `
+    type MarkdownRemark implements Node {
+      frontmatter: Frontmatter
+    }
+    type Frontmatter {
+      duration: String
+    }
+  `
+  createTypes(typeDefs)
+}
